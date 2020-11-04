effe175180c985dbf364210a1dd6bc9f06f31397
dlschoof <55069309+dlschoof@users.noreply.github.com>
2020-11-04 15:05:51 -0500

    Moved Assets around And Started GUI
    Moved character pics into their own folder, started GUI and added background and window images/icons

--- guiapp.py
+++ guiapp.py
@@ -1,6 +1,13 @@
 import random
+import tkinter as tk
+import requests
+from tkinter import font
+
+HEIGHT = 675 # Sets initial HEIGHT and WIDTH
+WIDTH = 800
 
 # A list of the characters, both with their image/character identifiers, and their full names as tuples - e.g. (name_name, Name Name)
+# All icons from https://www.mariowiki.com/Gallery:Super_Smash_Bros._Ultimate
 characters = [("mario", "Mario"), ("donkey_kong", "Donkey Kong"), ("link", "Link"), ("samus", "Samus"), ("dark_samus", "Dark Samus"), ("yoshi", "Yoshi"), ("kirby", "Kirby"), ("fox", "Fox"), 
 ("pikachu", "Pikachu"), ("luigi", "Luigi"), ("ness", "Ness"), ("captain_falcon", "Captain Falcon"), ("jigglypuff", "JigglyPuff"), ("peach", "Peach"), ("daisy", "Daisy"), ("bowser", "Bowser"), 
 ("ice_climbers", "Ice Climbers"), ("sheik", "Sheik"), ("zelda", "Zelda"), ("dr_mario", "Dr. Mario"), ("pichu", "Pichu"), ("falco", "Falco"), ("marth", "Marth"), ("lucina", "Lucina"), 
@@ -14,87 +21,22 @@ characters = [("mario", "Mario"), ("donkey_kong", "Donkey Kong"), ("link", "Link
 ("isabelle", "Isabelle"), ("incineroar", "Incineroar"), ("piranha_plant", "Piranha Plant"), ("joker", "Joker"), ("hero", "Hero"), ("banjo_kazooie", "Banjo & Kazooie"), ("terry", "Terry"), 
 ("byleth", "Byleth"), ("min_min", "Min Min"), ("steve", "Steve")]
 
-# The function to generate the list given number of characters, number of players, and the characters not being used
-def listGen(numberPlayers, numChars, notUsing):
-	if numberPlayers == 1: # If there is only one player
-		charChoose = []
+charCount = None # Initializes the character count, player count, list of player names, and list of not characters not bein used to nothing
+playerCount = None
+nameList = []
+notUsing = []
+specChar = ["mii_brawler", "mii_swordfighter", "mii_gunner", "piranha_plant", "joker", "hero", "banjo_kazooie", "terry", "byleth", "min_min", "steve"] # List of characters to ask about
 
-		while len(charChoose) < numChars: # While the characters chosen are less than number of characters to choose
-			ran = random.randint(0, len(characters) - 1) # Chooses a new character from list index
+root = tk.Tk() # Creates the window
+root.title("SmashFinder") # Sets the window name to Smash Finder
+root.iconbitmap('Assets/super_mario_question_box.ico') # Sets the window icon - from https://findicons.com/search/super-smash-bros
+root.resizable(False, False) # Makes the window not resizable
 
-			if characters[ran][0] not in notUsing and characters[ran] not in charChoose: # If the character chosen is not being not used and the character hasn't been chosen already
-				charChoose.append(characters[ran]) # Add this character to the chosen list
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH) # Creates canvas to set 
+canvas.pack()
 
-		return charChoose # Returns a single array of numChars characters
-	else:
-		charChoose = []
-		charChoose.append(listGen(1, numChars, notUsing)) # Recursively get two random lists and add them to a bigger list
-		charChoose.append(listGen(1, numChars, notUsing))
-		return charChoose # Returns a double array of lists of numChars characters
+background_image = tk.PhotoImage(file='Assets/smash_background.png') # Uploads background image from https://www.ssbwiki.com/Battlefield_(SSBU)
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
 
-# Prints out the lists based on the number of players, names of said player(s), and generated list(s)
-def outAnswers(num, names, ansCha):
-	print()
-	if num == 1: # If there is only one player, ansCha is a single array of characters
-		for cha in ansCha:
-			print(names[0] + " chooses " + cha[1] + "!") # For all characters in array, print the name of player with the charater name
-	else: # If there are two players, ansCha is double array of list of characters
-		for cha in ansCha[0]: 
-			print(names[0] + " chooses " + cha[1] + "!") # For all characters in list for p1, print name of p1 with each character name
-		print()
-		for cha2 in ansCha[1]:
-			print(names[1] + " chooses " + cha2[1] + "!") # For all characters in list for p2, print name of p2 with each character name
-	print()
-
-def main():
-	numPlay = -1
-	while numPlay < 1 or numPlay > 2:
-		numPlay = int(input("Are 1 or 2 people playing Smash? > ")) # Get the input of number of people playing smash (1 or 2)
-	
-	numCharChoice = 1
-	while numCharChoice != 5 and numCharChoice != 3:
-		numCharChoice = int(input("Would you like 3 or 5 characters? > ")) # Get input of number of characters playing with
-	
-	notUse = [] # List of characters
-	specChar = [["mii_brawler", 'H'], ["mii_swordfighter", 'H'], ["mii_gunner", 'H'], ["piranha_plant", 'H'], ["joker", 'H'], ["hero", 'H'], ["banjo_kazooie", 'H'], ["terry", 'H'], 
-	["byleth", 'H'], ["min_min", 'H'], ["steve", 'H']] # List of characters to ask about
-
-	for askChar in specChar: # For all characters to ask about
-		charName = None
-		for charAc in characters:
-			if askChar[0] == charAc[0]: # Finding the name of the characters
-				charName = charAc[1] # If found, set name to A[1]
-		while askChar[1] != 'Y' and askChar[1] != 'N':
-			askChar[1] = input("Do you have " + charName + " unlocked?\n    Please use Y or N > ") # Get input for Y or N if they have the character
-
-		if askChar[1] == 'N':
-			notUse.append(askChar[0]) # If they don't add it to list to not use
-
-
-	namesList = [] # List of character names
-
-	while len(namesList) == 0:
-		nameInput = input("Please enter the name of P1 > ") # Get p1 name
-		if nameInput is not None:
-			namesList.append(nameInput)
-
-	if numPlay == 2:
-		while len(namesList) < 2:
-			nameInput = input("Please enter the name of P2 > ") # If 2 characters, get p2 name
-			if nameInput is not None:
-				namesList.append(nameInput)
-	
-	ansCha = listGen(numPlay, numCharChoice, notUse) # Generate list with recieved values
-	outAnswers(numPlay, namesList, ansCha) # Print said list with recieved values
-
-	tryAgainAns = 'H'
-	while tryAgainAns != 'Y' and tryAgainAns != 'N':
-		tryAgainAns = input("Do you want to try a new team(s)?\n    Please use Y or N > ") # Finding if they want another list
-		if tryAgainAns == 'Y':
-			ansCha = listGen(numPlay, numCharChoice, notUse) # If they want another list, get and print another list
-			outAnswers(numPlay, namesList, ansCha)
-			tryAgainAns = 'H'
-
-
-if __name__ == "__main__":
-	main()
+root.mainloop()
