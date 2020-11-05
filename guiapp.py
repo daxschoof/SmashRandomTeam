a480d247b6761986622a3223ebc9b70fa029d90e
dlschoof <55069309+dlschoof@users.noreply.github.com>
2020-11-05 00:42:27 -0500

    Updated Background, Window, and initial Widgets
    Added Smash Background
    Added Window name and icon
    Added Frames for text and buttons for questions, initial button function handler

--- guiapp.py
+++ guiapp.py
@@ -3,8 +3,9 @@ import tkinter as tk
 import requests
 from tkinter import font
 
-HEIGHT = 675 # Sets initial HEIGHT and WIDTH
-WIDTH = 800
+# Sets initial HEIGHT and WIDTH
+HEIGHT = 450 
+WIDTH = 600
 
 # A list of the characters, both with their image/character identifiers, and their full names as tuples - e.g. (name_name, Name Name)
 # All icons from https://www.mariowiki.com/Gallery:Super_Smash_Bros._Ultimate
@@ -21,22 +22,73 @@ characters = [("mario", "Mario"), ("donkey_kong", "Donkey Kong"), ("link", "Link
 ("isabelle", "Isabelle"), ("incineroar", "Incineroar"), ("piranha_plant", "Piranha Plant"), ("joker", "Joker"), ("hero", "Hero"), ("banjo_kazooie", "Banjo & Kazooie"), ("terry", "Terry"), 
 ("byleth", "Byleth"), ("min_min", "Min Min"), ("steve", "Steve")]
 
-charCount = None # Initializes the character count, player count, list of player names, and list of not characters not bein used to nothing
+# Initializes the character count, player count, list of player names, and list of not characters not bein used to nothing
+charCount = None 
 playerCount = None
 nameList = []
 notUsing = []
 specChar = ["mii_brawler", "mii_swordfighter", "mii_gunner", "piranha_plant", "joker", "hero", "banjo_kazooie", "terry", "byleth", "min_min", "steve"] # List of characters to ask about
 
-root = tk.Tk() # Creates the window
-root.title("SmashFinder") # Sets the window name to Smash Finder
-root.iconbitmap('Assets/super_mario_question_box.ico') # Sets the window icon - from https://findicons.com/search/super-smash-bros
-root.resizable(False, False) # Makes the window not resizable
+# Handler for player number button
+def charCountHandler(butNum):
+	# Saving number of players
+	playerCount = butNum 
+	
+	# Removing player buttons
+	button1.place_forget() 
+	button2.place_forget()
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH) # Creates canvas to set 
+	# Adding buttons for 3 or 5 characters and placing them where button1 and button2 were
+	button3 = tk.Button(canvas, text='3 Characters', bg='#DE5B5B', fg='#1D0F7A', command=lambda: charCountHandler(1))
+	button3.place(relx=.275, rely=.65, relheight=.1, relwidth=.15, anchor='nw')
+
+	button4 = tk.Button(canvas, text='5 Characters', bg='#DE5B5B', fg='#1D0F7A', command=lambda: charCountHandler(2))
+	button4.place(relx=.575, rely=.65, relheight=.1, relwidth=.15, anchor='nw')
+
+	# Setting the question label to ask about # of characters
+	ques_label.config(text = 'How many characters are you playing with?')
+
+# Creates the window, sets the window name to Smash Finder, sets the window icon, and making the window not resizable
+root = tk.Tk() 
+root.title("SmashRandomTeam")  
+root.iconbitmap('Assets/super_mario_question_box.ico') # https://findicons.com/search/super-smash-bros
+root.resizable(False, False) 
+
+# Creates canvas to set
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)  
 canvas.pack()
 
-background_image = tk.PhotoImage(file='Assets/smash_background.png') # Uploads background image from https://www.ssbwiki.com/Battlefield_(SSBU)
-background_label = tk.Label(root, image=background_image)
+# Uploads background image from https://www.ssbwiki.com/Battlefield_(SSBU), then sets the image/label to the background image then places it
+background_image = tk.PhotoImage(file='Assets/smash_background.png') 
+background_label = tk.Label(canvas, image=background_image, anchor='n') 
 background_label.place(relwidth=1, relheight=1)
+
+# Creates a frame for the welcome text
+frame_text = tk.Frame(root, bg='#FF0000', bd=5) 
+frame_text.place(relx=.15, rely=.15, relwidth=.7, relheight=.15)
+
+# Defining the text to go into the welcome text
+welcome_text = 'Hello! Welcome to SmashRandomTeam! We are going to get you the best random team, but first we do need some info from you.'
+welcome_text = welcome_text + ' Please click on the button that applies for your teams, and we can get started!'
+
+# Makes a label for the welcome text
+welcome_label = tk.Label(frame_text, bg='#ffffff', bd=1, text=welcome_text, wraplength=400, font=('Courier', 8)) 
+welcome_label.place(relwidth=1, relheight=1)
+
+# Adds a frame for the text asking the question relating to the button
+frame_ques = tk.Frame(root, bg='#FF0000', bd=5) 
+frame_ques.place(relx=.225, rely=.4, relwidth=.55, relheight=.1)
+
+# Adds question text
+ques_label = tk.Label(frame_ques, bg='#ffffff', bd=1, text='How many players do you have?', wraplength=400, font=('Courier', 8)) 
+ques_label.place(relwidth=1, relheight=1)
+
+# Button for 1 player
+button1 = tk.Button(canvas, text='1 Hero', bg='#DE5B5B', fg='#1D0F7A', command=lambda: charCountHandler(1)) 
+button1.place(relx=.275, rely=.65, relheight=.1, relwidth=.15, anchor='nw')
+
+# Button for 2 players
+button2 = tk.Button(canvas, text='2 Warriors', bg='#DE5B5B', fg='#1D0F7A', command=lambda: charCountHandler(2)) 
+button2.place(relx=.575, rely=.65, relheight=.1, relwidth=.15, anchor='nw')
 
 root.mainloop()
